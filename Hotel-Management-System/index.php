acf98e770be41c1981efd41d9aeb818e50fee1cd
Shweta Lale <115721205+shwetalale25@users.noreply.github.com>
2023-08-19 14:46:36 +0530

    Add files via upload

---
+++ Hotel-Management-System/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+include('header.php');
+include('admin/db_connect.php');
+
+$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
+foreach ($query as $key => $value) {
+  if (!is_numeric($key))
+    $_SESSION['setting_' . $key] = $value;
+}
+?>
+
+<style>
+  header.masthead {
+    background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  body {
+    background-color: #f2caffb3;
+  }
+
+  footer {
+    background-color: #73359f;
+    margin-top: 2rem;
+  }
+</style>
+
+<body id="page-top">
+  <!-- Navigation-->
+  <!-- <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body text-white">
+    </div>
+  </div> -->
+  <nav class="navbar navbar-expand-lg  fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="./">
+        <?php echo $_SESSION['setting_hotel_name'] ?>
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+        aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=list">Rooms</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <?php
+  $page = isset($_GET['page']) ? $_GET['page'] : "home";
+  include $page . '.php';
+  ?>
+
+
+  <div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Confirmation</h5>
+        </div>
+        <div class="modal-body">
+          <div id="delete_content"></div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title"></h5>
+        </div>
+        <div class="modal-body">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" id='submit'
+            onclick="$('#uni_modal form').submit()">Save</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="py-5">
+    <div class="container">
+      <div class="large text-center  text-white ">Hotel Mangement system</div>
+    </div>
+  </footer>
+
+  <?php include('footer.php') ?>
+</body>
+
+<?php $conn->close() ?>
+
+</html>
